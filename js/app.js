a74d095685aaefd7bb221890e16180bb1f3964be
ramafikri02 <ramafikri02@gmail.com>
2020-10-15 17:02:31 +0700

    menambahkan animasi saat back ke menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function openChat() {
-    document.getElementById("myChat").style.display = "none";
-    document.getElementById("myMessage").style.display = "block";
+    document.getElementById("myChat").style.display = "block";
+    document.getElementById("myMessage").style.display = "none";
     document.getElementById("openChat").style.display = "none";
 }
 
@@ -17,5 +17,8 @@ function closeForm() {
 
 function backMessage() {
     document.getElementById("myChat").style.display = "block";
+    document.getElementById("myChat").style.animation = "none";
+    document.getElementById("logo").style.animation = "fadeIn .3s ease-in";
+    document.getElementById("content").style.animation = "fadeInUp .3s ease-in";
     document.getElementById("myMessage").style.display = "none";
 }
