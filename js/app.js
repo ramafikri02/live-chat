49a7c1468275fa14ed7625d999a052312e822464
ramafikri02 <ramafikri02@gmail.com>
2020-10-15 11:04:08 +0700

    Perbaikan directory file

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function openChat() {
+    document.getElementById("myChat").style.display = "block";
+    document.getElementById("myMessage").style.display = "none";
+    document.getElementById("openChat").style.display = "none";
+}
+
+function openMessage() {
+    document.getElementById("myMessage").style.display = "block";
+    document.getElementById("myChat").style.display = "none";
+}
+
+function closeForm() {
+    document.getElementById("myChat").style.display = "none";
+    document.getElementById("myMessage").style.display = "none";
+    document.getElementById("openChat").style.display = "block";
+}
+
+function backMessage() {
+    document.getElementById("myChat").style.display = "block";
+    document.getElementById("myMessage").style.display = "none";
+}
