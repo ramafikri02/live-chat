437407b8346c2c36ed56f1f550a6cc0ecedb5496
ramafikri02 <ramafikri02@gmail.com>
2020-10-16 14:17:02 +0700

    Partial CSS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 function openChat() {
     document.getElementById("myChat").style.display = "block";
+    document.getElementById("logo").style.animation = "none";
+    document.getElementById("content").style.animation = "none";
+    document.getElementById("myChat").style.animation = "fadeInUpLeft .5s ease-in-out";
     document.getElementById("myMessage").style.display = "none";
     document.getElementById("openChat").style.display = "none";
 }
@@ -22,3 +25,11 @@ function backMessage() {
     document.getElementById("content").style.animation = "fadeInUp .3s ease-in";
     document.getElementById("myMessage").style.display = "none";
 }
+
+onkeydown = function (e) {
+    if (e.keyCode == e.DOM_VK_ESCAPE) {
+        document.getElementById("myChat").style.display = "none";
+        document.getElementById("myMessage").style.display = "none";
+        document.getElementById("openChat").style.display = "block";
+    }
+};
